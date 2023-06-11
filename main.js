202722028291a1f35fe0903146b3dd7b0c076d86
Kashif Ali <kashifali@ophycare.com>
2023-06-11 22:46:55 +0400

    sprinkles on click

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const particlesArray = [];
+const numberOfParticles = 100;
+
 console.log(ctx);
 
 window.addEventListener("resize", (event) => {
@@ -18,22 +21,59 @@ const mouse = {
 canvas.addEventListener("click", (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
-  drawCirle();
+  for (let i = 0; i < 10; i++) particlesArray.push(new Particle());
 });
 
 canvas.addEventListener("mousemove", (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
-  drawCirle();
 });
 
-function drawCirle() {
-  ctx.fillStyle = "red";
-  ctx.strokeStyle = "red";
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-  ctx.stroke();
-  ctx.fill();
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    //this.x = Math.random() * canvas.width;
+    //this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.02;
+  }
+
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
 
-drawCirle();
+class Effect {
+  constructor() {}
+}
+
+function handleParticles() {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+    if (particlesArray[i].size <= 0.3) {
+      particlesArray.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+console.log(particlesArray);
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  requestAnimationFrame(animate);
+}
+
+animate();
